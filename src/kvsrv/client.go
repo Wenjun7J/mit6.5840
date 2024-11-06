ac3c61a53ed2828965104fd9edbecdbfd809bcf4
Wenjun7J <wenjun7j@gmail.com>
2024-11-06 19:23:28 +0800

    finish lab2

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,13 +1,18 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+	"time"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
 	// You will have to modify this struct.
+	id    int64
+	seqid int
 }
 
 func nrand() int64 {
@@ -21,6 +26,8 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.server = server
 	// You'll have to add code here.
+	ck.id = nrand()
+	ck.seqid = 0
 	return ck
 }
 
@@ -37,7 +44,17 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	args := GetArgs{Key: key, ClerkId: ck.id, Seqid: ck.seqid + 1}
+	reply := GetReply{}
+	for {
+		if ok := ck.server.Call("KVServer.Get", &args, &reply); !ok {
+			//fmt.Printf("call Get failed %v %v, %v %v\n", ck.id, ck.seqid, ok, reply.OK)
+		} else {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -50,7 +67,18 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
-	return ""
+	args := PutAppendArgs{Key: key, Value: value, ClerkId: ck.id, Seqid: ck.seqid}
+	reply := PutAppendReply{}
+	for {
+		if ok := ck.server.Call("KVServer."+op, &args, &reply); !ok {
+			//fmt.Printf("call PutAppend failed %v %v, %v %v\n", ck.id, ck.seqid, ok, reply.OK)
+		} else {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	ck.seqid++
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,24 +14,73 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	data        sync.Map
+	seqid       sync.Map
+	reply_cache sync.Map
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	if seqId, ok := kv.seqid.Load(args.ClerkId); ok {
+		if args.Seqid > seqId.(int) {
+			kv.reply_cache.Delete(args.ClerkId)
+		}
+	}
+	val, _ := kv.data.LoadOrStore(args.Key, "")
+	reply.Value = val.(string)
 }
+
+// func bToMb(b uint64) uint64 {
+// 	return b / 1024 / 1024
+// }
+// func printMemStats() {
+// 	var m runtime.MemStats
+// 	runtime.ReadMemStats(&m)
+
+// 	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+// 	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+// 	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+// 	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+// }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	seqId, _ := kv.seqid.LoadOrStore(args.ClerkId, 0)
+	if args.Seqid == seqId.(int) {
+		kv.data.Store(args.Key, args.Value)
+		kv.seqid.Store(args.ClerkId, seqId.(int)+1)
+		kv.reply_cache.Delete(args.ClerkId)
+	} else if args.Seqid == seqId.(int)-1 {
+		//do nothing
+	} else {
+		//something oops...
+	}
+	//fmt.Printf("put: %v %v %v (%v : %v)\n", args.ClerkId, args.Seqid, seqId, args.Key, args.Value)
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	seqId, _ := kv.seqid.LoadOrStore(args.ClerkId, 0)
+	if args.Seqid == seqId.(int) {
+		val, _ := kv.data.LoadOrStore(args.Key, "")
+		reply.Value = val.(string)
+		kv.reply_cache.Store(args.ClerkId, val)
+		kv.seqid.Store(args.ClerkId, seqId.(int)+1)
+		kv.data.Store(args.Key, reply.Value+args.Value)
+	} else if args.Seqid == seqId.(int)-1 {
+		val, _ := kv.reply_cache.Load(args.ClerkId)
+		reply.Value = val.(string)
+	} else {
+		//something oops...
+	}
+	//fmt.Printf("append: %v %v %v (%v : %v)\n", args.ClerkId, args.Seqid, seqId, args.Key, args.Value)
 }
 
 func StartKVServer() *KVServer {
